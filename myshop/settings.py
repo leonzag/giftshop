ac8361250459184134c2da4151e1b26e700e25e2
Leonid Zagumennov <leonzag997@gmail.com>
2025-06-29 23:51:31 +0300

    ~ explicitly app config: dotted AppConfig path

--- myshop/settings.py
+++ myshop/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "shop",
-    "cart",
-    "orders",
+    "shop.apps.ShopConfig",
+    "cart.apps.CartConfig",
+    "orders.apps.OrdersConfig",
 ]
 
 MIDDLEWARE = [
