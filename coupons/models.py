274cad2dae86fb658b6d0d77780a4d0a077dde3a
Leonid Zagumennov <leonzag997@gmail.com>
2025-07-06 02:04:01 +0300

    + orders: explicitly model ID

--- coupons/models.py
+++ coupons/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 
 class Coupon(models.Model):
+    id = models.BigAutoField(primary_key=True)
     code = models.CharField(max_length=50, unique=True, verbose_name="Код купона")
     valid_from = models.DateTimeField(verbose_name="Действителен с")
     valid_to = models.DateTimeField(verbose_name="Действителен до")
