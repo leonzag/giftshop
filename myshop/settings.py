0f294eba0f2c72111c8a50510a09e08eb184385f
Leonid Zagumennov <leonzag997@gmail.com>
2025-07-05 19:30:36 +0300

    + cart: cart button in header

--- myshop/settings.py
+++ myshop/settings.py
@@ -65,6 +65,7 @@ TEMPLATES = [
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
                 "shop.context_processors.categories",
+                "cart.context_processors.cart",
             ],
             "libraries": {
                 "filters": "filters.filters",
