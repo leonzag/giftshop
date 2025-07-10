c767e072c0af3581b685f10625567304d6d78065
Leonid Zagumennov <leonzag997@gmail.com>
2025-07-11 00:38:51 +0300

    ~ project: яРусский lang fix

--- orders/models.py
+++ orders/models.py
@@ -17,16 +17,16 @@ class Order(models.Model):
         blank=True,
         null=True,
     )
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50, verbose_name="Имя")
+    last_name = models.CharField(max_length=50, verbose_name="Фамилия")
     email = models.EmailField()
-    address = models.CharField(max_length=250)
-    postal_code = models.CharField(max_length=20)
-    city = models.CharField(max_length=100)
-    phone_number = models.CharField(max_length=20)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-    paid = models.BooleanField(default=False)
+    address = models.CharField(max_length=250, verbose_name="Адрес")
+    postal_code = models.CharField(max_length=20, verbose_name="Индекс")
+    city = models.CharField(max_length=100, verbose_name="Город")
+    phone_number = models.CharField(max_length=20, verbose_name="Телефон")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Создано")
+    updated = models.DateTimeField(auto_now=True, verbose_name="Обновлено")
+    paid = models.BooleanField(default=False, verbose_name="Оплачено")
     coupon = models.ForeignKey(
         Coupon,
         related_name="orders",
@@ -44,6 +44,8 @@ class Order(models.Model):
         indexes = [
             models.Index(fields=["-created"]),  # Индекс для поля created
         ]
+        verbose_name = "Заказ"
+        verbose_name_plural = "Заказы"
 
     def __str__(self):
         return f"Order {self.id}"
--- shop/models.py
+++ shop/models.py
@@ -3,13 +3,13 @@ from django.urls import reverse
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=200, db_index=True)
+    name = models.CharField(max_length=200, db_index=True, verbose_name="Имя")
     slug = models.SlugField(max_length=200, unique=True)
 
     class Meta:
         ordering = ("name",)
-        verbose_name = "category"
-        verbose_name_plural = "categories"
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
 
     def __str__(self) -> str:
         return self.name
@@ -20,20 +20,24 @@ class Category(models.Model):
 
 class Product(models.Model):
     id = models.BigAutoField(primary_key=True)
-    category = models.ForeignKey(Category, related_name="products", on_delete=models.SET_NULL, blank=True, null=True)
-    name = models.CharField(max_length=200, db_index=True)
+    category = models.ForeignKey(
+        Category, related_name="products", on_delete=models.SET_NULL, blank=True, null=True, verbose_name="Категория"
+    )
+    name = models.CharField(max_length=200, db_index=True, verbose_name="Имя")
     slug = models.SlugField(max_length=200, db_index=True)
-    image = models.ImageField(upload_to="products/%Y/%m/%d", blank=True)
-    description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    stock = models.PositiveIntegerField()
-    available = models.BooleanField(default=True)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    image = models.ImageField(upload_to="products/%Y/%m/%d", blank=True, verbose_name="Изображение")
+    description = models.TextField(blank=True, verbose_name="Описание")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена")
+    stock = models.PositiveIntegerField(verbose_name="Остаток")
+    available = models.BooleanField(default=True, verbose_name="Доступно")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Создано")
+    updated = models.DateTimeField(auto_now=True, verbose_name="Обновлено")
 
     class Meta:
         ordering = ("name",)
         indexes = [models.Index(fields=["id", "slug"])]
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
 
     def __str__(self) -> str:
         return self.name
