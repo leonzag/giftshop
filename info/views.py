95e4f861ad554e1f3fedfc5b425732fb103425a8
Leonid Zagumennov <leonzag997@gmail.com>
2025-07-10 20:32:36 +0300

    + info: implement customer feedback message

--- info/views.py
+++ info/views.py
@@ -1,4 +1,29 @@
 from django.shortcuts import render
 
+from .forms import ContactForm
+from .models import ContactMessage
+from .settings import CONTACT_EMAIL
+from .tasks import send_contact_email
+
+
 def info(request):
-    return render(request, 'info/info.html')
+    form = ContactForm()
+    if request.method != "POST":
+        return render(request, "info/info.html", {"form": form})
+
+    form = ContactForm(request.POST)
+    if not form.is_valid():
+        form = ContactForm()
+        return render(request, "info/info.html", {"form": form})
+
+    contact_message = ContactMessage.objects.create(
+        name=form.cleaned_data["name"],
+        email=form.cleaned_data["email"],
+        message=form.cleaned_data["message"],
+    )
+    send_contact_email.delay(
+        contact_message.id,
+        CONTACT_EMAIL,
+    )
+
+    return render(request, "info/contact_success.html")
